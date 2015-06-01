49c363b5ba0a186a0dd8756859f9a84dc4d7d747
Vibhav Pant <vibhavp@gmail.com>
2015-06-02 01:27:27 +0530

    Use R5RS names for predicates.

--- src/token.c
+++ src/token.c
@@ -30,11 +30,11 @@
 #include <stdbool.h>
 
 static const char *builtin_syms[22] =  {"and", "car", "cdr", "cond", "cons",
-                                        "define", "eval", "lambda", "not",
-                                        "or", "print", "quote", "integer_p",
-                                        "float_p", "number_p", "string_p",
-                                        "symbol_p", "list_p", "lambda_p",
-                                        "boolean_p", "eqv_p", "equal_p"};
+                                        "define", "eval", "lambda"
+                                        "or", "print", "quote", "integer?",
+                                        "float?", "number?", "string?",
+                                        "symbol?", "list?", "lambda?",
+                                        "boolean?", "eqv?", "equal?"};
 enum tok_type {
   TOK_INT,
   TOK_FLOAT,
