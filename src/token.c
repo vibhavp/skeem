c287630a87bec677325a418bbeac7c38621f74e0
Vibhav Pant <vibhavp@gmail.com>
2015-06-05 07:18:09 +0530

    Remove builtin_syms definition.

--- src/token.c
+++ src/token.c
@@ -28,12 +28,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-static const char *builtin_syms[22] =  {"and", "car", "cdr", "cond", "cons",
-                                        "define", "eval", "lambda", "not",
-                                        "or", "print", "quote", "integer?",
-                                        "float?", "number?", "string?",
-                                        "symbol?", "list?", "lambda?",
-                                        "boolean?", "eqv?", "equal?"};
 
 enum tok_type {
   TOK_INT,
