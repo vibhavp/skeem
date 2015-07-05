fbb9213f88a81b3bab3cd447c77a236a2c230f94
Vibhav Pant <vibhavp@gmail.com>
2015-07-05 22:34:26 +0530

    Dont print newline.

--- src/skeem.c
+++ src/skeem.c
@@ -92,7 +92,7 @@ int main(int argc, char **argv) {
     obj = eval(obj);
 
     if (stream == stdin) {
-      printf("\n=> ");
+      printf("=> ");
       fflush(stdout);
       print_obj(obj, stdout);
       putchar('\n');
